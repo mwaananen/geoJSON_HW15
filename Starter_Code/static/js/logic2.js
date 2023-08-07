3fccf5564167ebd904069f204815fac3591ab340
mwaananen <m.r.waananen@gmail.com>
2023-08-06 19:56:17 -0500

    Assignment 15

---
+++ Starter_Code/static/js/logic2.js
@@ -0,0 +1,77 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function getColor(depth) {
+  if (
+      depth > 90
+  ) {return "red"}
+  else if (
+      depth > 70
+  ) {return "orangered"}
+  else if (
+      depth > 50
+  ) {return "orange"}
+  else if (
+      depth > 30
+  ) {return "yellow"}
+  else if (
+      depth > 10
+  ) {return "green"}
+  else if (
+      depth > -10
+  ) {return "limegreen"}
+};
+
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h2>Location: ${feature.properties.place}</h2> <hr> <h3>Magnitude: ${feature.properties.mag}</h3>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature, 
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: feature.properties.mag * 5,
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    }
+  })
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+};
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 4,
+    layers: [earthquakes, street]
+  });
+
+
+}
